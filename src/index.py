25711a0ad3c3c55fa4c21e35ad8cb190c941f4a7
Touko Hänninen <touko.j.hanninen@student.jyu.fi>
2025-11-08 15:01:06 +0200

    pylint test

--- src/index.py
+++ src/index.py
@@ -9,19 +9,32 @@ def main():
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
-    print("Olut getterit:")
-    print(f"saldo = {olutta.saldo}")
-    print(f"tilavuus = {olutta.tilavuus}")
-    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
+    tulosta_olut_getterit(olutta)
 
+    mehu_setterit(mehua)
+
+    tulosta_virhetilanteita()
+
+    lisataan_varastoihin(mehua, olutta)
+
+    varastoista_saadaan(mehua, olutta)  
+
+def tulosta_olut_getterit(varasto):
+    print("Olut getterit:")
+    print(f"saldo = {varasto.saldo}")
+    print(f"tilavuus = {varasto.tilavuus}")
+    print(f"paljonko_mahtuu = {varasto.paljonko_mahtuu()}")
+
+def mehu_setterit(varasto):
     print("Mehu setterit:")
     print("Lisätään 50.7")
-    mehua.lisaa_varastoon(50.7)
-    print(f"Mehuvarasto: {mehua}")
+    varasto.lisaa_varastoon(50.7)
+    print(f"Mehuvarasto: {varasto}")
     print("Otetaan 3.14")
-    mehua.ota_varastosta(3.14)
-    print(f"Mehuvarasto: {mehua}")
+    varasto.ota_varastosta(3.14)
+    print(f"Mehuvarasto: {varasto}")
 
+def tulosta_virhetilanteita():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,6 +44,7 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def lisataan_varastoihin(mehua, olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
@@ -41,6 +55,7 @@ def main():
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def varastoista_saadaan(mehua, olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
@@ -52,7 +67,6 @@ def main():
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
-
 
 if __name__ == "__main__":
     main()
